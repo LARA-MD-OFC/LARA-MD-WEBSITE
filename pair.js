15ea58cb2f3c5dd1484c74d34b5b300c566c57f7
LARA-MD-OFC <laravofficialbot@gmail.com>
2025-07-23 17:47:54 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -81,11 +81,11 @@ router.get("/", async (req, res) => {
               ""
             );
 
-            const sid = `*ROBIN [The powerful WA BOT]*\n\n👉 ${string_session} 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*wa.me/message/WKGLBR2PCETWD1*\n\n*You can join my whatsapp group*\n\n*https://chat.whatsapp.com/GAOhr0qNK7KEvJwbenGivZ*`;
-            const mg = `🛑 *Do not share this code to anyone* 🛑`;
+            const sid = `*LARA-MD  [The powerful WA BOT]*\n\n👉 ${string_session} 👈\n\n*This Is The your Session ID, Copy This ID And Aaste Into Config.js File*\n\n*You Can Ask Any Question Using This Number*\n\n*94720522884*\n\n*You can join my whatsapp group*\n\n*https://whatsapp.com/channel/0029Vb5ygc31SWsvGfvMTJ0c*`;
+            const mg = `*DO NOT SHARE THIS CODE TO ANYONE*`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
-                url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
+                url: "https://github.com/LARA-MD-OFC/Lara-MD-Help/blob/main/Connected%20.png?raw=true",
               },
               caption: sid,
             });
